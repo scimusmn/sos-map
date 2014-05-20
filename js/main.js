bafcfabdb72becf81c04325ed0ab4d45c5727a7f
bryan kennedy <bkennedy@smm.org>
2014-05-20 11:10:22 -0500

    Adding a second marker for Irvine Park
    Just checking to see how multiple markers work.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var map = L.map('map').setView([44.9425857,-93.0986076], 15);
 var marker = L.marker([44.9425857,-93.0986076]).addTo(map);
 marker.bindPopup("<b>Science Museum of Minnesota</b><br>The conference will take place here.").openPopup();
+var irvinePark = L.marker([44.9409036,-93.1027865]).addTo(map);
+
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/bkennedy.i9o0b4hb/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
