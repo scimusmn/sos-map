f6a9eca18a148ece4f6e01468fba3c448182d75e
bryan kennedy <bkennedy@smm.org>
2014-05-20 11:02:28 -0500

    Creating a map and linking it to my MapBox project
    This is just a basic map focused on downtown Saint Paul.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+var map = L.map('map').setView([44.9425857,-93.0986076], 15);
+L.tileLayer('http://{s}.tiles.mapbox.com/v3/bkennedy.i9o0b4hb/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(map);
