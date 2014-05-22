6985730e4265b0c3ac545f0962c38d0869e018f4
bryan kennedy <bkennedy@smm.org>
2014-05-22 11:16:50 -0500

    Better formatting for JS
    I'd ratehr be doing this in a big template but I don't have time to get
    it working.

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ smm.bindPopup("<b>Science Museum of Minnesota</b>" +
 var irvinePark = L.marker([44.9409036,-93.1027865]).addTo(map);
 irvineParkImg = 'img/3827710401_1e7e48f132_z.jpg';
 irvinePark.bindPopup('<h2>Irvine Park</h2>' +
-                     '<div class="image-container">' +
-                     '  <img class="marker-image" width="200" src="' + irvineParkImg + '" />' +
-                     '  <div class="credit"><a href="https://flic.kr/p/6Qf1WV">Sharyn Morrow</a> / CC-BY-NC-ND-2.0</div>' +
-                     '</div>' +
-                     'Sciurus wisi ad tincidunt duis luptatum eros ad volutpat dolor, praesent esse veniam wisi nonummy ut. Iusto tation, nostrud blandit nibh luptatum lobortis ea nostrud consequatvel blandit enim in hendrerit dolore sit. Sciurus at amet exerci feugiat et laoreet nulla consequat, ut nulla euismod feugait esse qui. Autem, facilisi enim nulla ut illum autem eros volutpat autem ea aliquip facilisis feugiat odio accumsan lobortis facilisi?',
-                    {minWidth: '600'});
+    '<div class="image-container">' +
+    '  <img class="marker-image" width="200" src="' + irvineParkImg + '" />' +
+    '  <div class="credit"><a href="https://flic.kr/p/6Qf1WV">Sharyn Morrow</a> / CC-BY-NC-ND-2.0</div>' +
+    '</div>' +
+    'This 19th century square with its fountain and mature oaks surrounded by Victorian houses is a Saint Paul treasure. It is all the more precious for avoiding the wrecking ball in the 1970s. Stand in middle of the park, do a 360, then imagine the scene if those who wanted an industrial park had gotten their wish.',
+    {minWidth: '600'});
 
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/bkennedy.i9o0b4hb/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
