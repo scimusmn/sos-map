625c2d62543ae191efd5c69609ee5095df640228
bryan kennedy <bkennedy@smm.org>
2014-06-05 12:05:23 -0500

    Moving map points to a GeoJSON format
    This will make it easier to pull this content into the map without lots
    of extraneous boilerplate code.

---
+++ js/points.js
@@ -0,0 +1,414 @@
+var mapPoints = {
+    "type": "FeatureCollection",
+    "features": [
+
+    /**
+     * SMM - probably delete this point
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Science Museum of Minnesota",
+            "img": "img/SMM_2-small.jpg",
+            "creditLink": "https://flic.kr/p/6Qf1WV",
+            "creditName": "Sharyn Morrow",
+            "creditLicense": "CC-BY-NC-ND-2.0",
+            "bodyCopy": "The conference will take place here.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.09895992279053,
+                44.942536529903876
+            ]
+        }
+    },
+
+    /**
+     * Irvine Park
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Irvine Park",
+            "img": "img/3827710401_1e7e48f132_z.jpg",
+            "creditLink": "https://flic.kr/p/6Qf1WV",
+            "creditName": "Sharyn Morrow",
+            "creditLicense": "CC-BY-NC-ND-2.0",
+            "bodyCopy": "This 19th century square with its fountain and mature oaks surrounded by Victorian houses is a Saint Paul treasure. It is all the more precious for avoiding the wrecking ball in the 1970s. Stand in middle of the park, do a 360, then imagine the scene if those who wanted an industrial park had gotten their wish.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.1027865,
+                44.9409036
+            ]
+        }
+    },
+
+    /**
+     * Mears Park
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Mears Park",
+            "img": "img/2905454937_29fba63026_z.jpg",
+            "creditLink": "https://flic.kr/p/5qKdwD",
+            "creditName": "Teresa Boardman",
+            "creditLicense": "CC-BY-NC-ND-2.0",
+            "bodyCopy": "Sure, the stream is artificial but the tumbling waters shaded by an urban forest make for a very nice retreat in the heart of the city. The restaurants and bars lining the north side of the park make bar hopping fun and convenient.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.08779,
+                44.94935
+            ]
+        }
+    },
+
+    /**
+     * Candy Land
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Candy Land",
+            "img": "img/3725900249_a2a720f28b_z.jpg",
+            "creditLink": "https://flic.kr/p/6Ffdkt",
+            "creditName": "AJ Wms",
+            "creditLicense": "CC-BY-NC-2.0",
+            "bodyCopy": "This tiny shop is stuffed with fat, salt and sugar. How great is that? Whaaat, you’ve never had a squirrel turd? Well, here is your chance. Remember – either buy enough for everyone or surreptitiously enjoy your delicious treats.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.09642,
+                44.94745
+            ]
+        }
+    },
+
+    /**
+     * Landmark Center
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Landmark Center",
+            "img": "img/29999783_45a66e4636_z.jpg",
+            "creditLink": "https://flic.kr/p/3dktd",
+            "creditName": "Michael Hicks",
+            "creditLicense": "CC-BY-2.0",
+            "bodyCopy": "Walk around the exterior of this exquisite building, then stroll through its interior. The plan in the 1970s was to demolish this former federal courthouse and replace it with a surface parking lot. Pretty crazy stupid, right?",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.09713,
+                44.94562
+            ]
+        }
+    },
+
+    /**
+     * Restaraunt Row
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Restaraunt Row",
+            "img": "img/7329203136_201d9cd733_z.jpg",
+            "creditLink": "https://flic.kr/p/caE5Kj",
+            "creditName": "jpellgen",
+            "creditLicense": "CC-BY-NC-ND-2.0",
+            "bodyCopy": "From one end of St. Peter Street to the other, you can enjoy Kurdish, Thai, French, Italian, Japanese and classic American cuisine. They’re all fun but my favorite is Meritage. It has it all – delicious food, wonderful service, great atmosphere and an intimate bar.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.09678,
+                44.94654
+            ]
+        }
+    },
+
+
+    /**
+     * Fitzgerald Theater
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Fitzgerald Theater",
+            "img": "img/3567082778_c8c39847fb_z.jpg",
+            "creditLink": "https://flic.kr/p/6rderd",
+            "creditName": "On Being",
+            "creditLicense": "CC-BY-NC-ND-2.0",
+            "bodyCopy": "Yep, this is where the magic of \"A Prairie Home Companion\" takes place. It also is home to \"Wits\" – an up and coming radio show.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.0972508,
+                44.9490803
+            ]
+        }
+    },
+
+
+    /**
+     * Nice Ride
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Nice Ride",
+            "img": "img/7812483572_0ee2b3713f_b.jpg",
+            "creditLink": "https://flic.kr/p/cUn2bC",
+            "creditName": "m01229",
+            "creditLicense": "CC-BY-2.0",
+            "bodyCopy": "Simply take a bike when you need one and return it to any station in the system when you are done. There is a Nice Ride station on the Science Museum’s plaza and others scattered elsewhere in downtown Saint Paul. Use a bike to expand the range of your explorations to include Cathedral Hill, Summit Avenue and the Mississippi River.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.09796,
+                44.94300
+            ]
+        }
+    },
+
+
+    /**
+     * Custom House
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Custom House",
+            "img": "img/14106289332_28558b390c_b.jpg",
+            "creditLink": "https://flic.kr/p/nuwqn3",
+            "creditName": "Michael Hicks",
+            "creditLicense": "CC-BY-2.0",
+            "bodyCopy": "The U.S. Postal Service freed up this building two years ago for redevelopment. As with many cities, downtown Saint Paul is seeing a resurgence in urban living. Work will begin shortly to convert this building into apartments, condos, a hotel and mixed retail. ",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.08783,
+                44.94576
+            ]
+        }
+    },
+
+
+    /**
+     * St. Paul District Heating and Cooling
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "St. Paul District Heating and Cooling",
+            "img": "img/5313163891_c5d0cd4734_b.jpg",
+            "creditLink": "https://flic.kr/p/96vmvZ",
+            "creditName": "brnpttmn",
+            "creditLicense": "CC-BY-NC-SA-2.0",
+            "bodyCopy": "One of the city’s best kept secrets, this plant generates 30 MW of electricity largely using waste wood collected mostly from the Twin Cities metropolitan area and then heats and cools 80% and 60% respectively of the downtown. It is the largest district system in the U.S. ",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.09667,
+                44.94291
+            ]
+        }
+    },
+
+
+    /**
+     * Central library
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "James J. Hill Reference Library / Saint Paul Central Library",
+            "img": "img/2058870277_6e84c4e9c4_b.jpg",
+            "creditLink": "https://flic.kr/p/48Wfpp",
+            "creditName": "Jeremy Noble",
+            "creditLicense": "CC-BY-2.0",
+            "bodyCopy": "The reference library was recently named 'the greatest historical treasure' among the nation’s library in a nationwide photo contest. Check it out and then go next door to explore the central library which went through a top-to-bottom restoration in 2002.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.09705,
+                44.94380
+            ]
+        }
+    },
+
+
+    /**
+     * Lunds
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Lunds",
+            "img": "img/14205050861_a6ff5d214b_b.jpg",
+            "creditLink": "https://flic.kr/p/nDfAJ2",
+            "creditName": "Michael Hicks",
+            "creditLicense": "CC-BY-2.0",
+            "bodyCopy": "Yeah, I know it’s just a supermarket but a rather nice one.  More importantly, it is the first in downtown in a very long time, an encouraging harbinger for the continued growth in downtown’s residential population. Pick up a snack. ",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.0950558,
+                44.951411
+            ]
+        }
+    },
+
+
+    /**
+     * Union Depot
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Union Depot",
+            "img": "img/8351124559_921d3673f6_b.jpg",
+            "creditLink": "https://flic.kr/p/dHXGoc",
+            "creditName": "Jeb",
+            "creditLicense": "CC-BY-NC-SA-2.0",
+            "bodyCopy": "See what $250,000,000 can do? The recent refurbishment of this early 20th century train station allowed passenger train service to return to downtown St. Paul for the first time in over 40 years. Now, if only there was high-speed rail service to Chicago...",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.0855644,
+                44.9471505
+            ]
+        }
+    },
+
+
+    /**
+     * Upper Landing
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Upper Landing",
+            "img": "img/2833426603_dc0066e46d_b.jpg",
+            "creditLink": "https://flic.kr/p/5jo41P",
+            "creditName": "Greg Matthews",
+            "creditLicense": "CC-BY-SA-2.0",
+            "bodyCopy": "So named because it was the upstream counterpart to the Lower Steamboat Landing, this once bustling river port now is a park with walking/bike paths that you can take long distances either up or downstream.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.0993906,
+                44.9402909
+            ]
+        }
+    },
+
+
+    /**
+     * Mademoiselle Miel
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Mademoiselle Miel",
+            "img": "img/6882550156_4abb1d6f22_o.jpg",
+            "creditLink": "https://flic.kr/p/bubSnS",
+            "creditName": "mamichan",
+            "creditLicense": "CC-BY-NC-2.0",
+            "bodyCopy": "Honey from rooftop hives on Saint Paul buildings is used to create bon–bons. The kitchen/store is only open Friday afternoons but the bon-bons can be purchased at the Heartland Restaurant and Farm Direct Market at 5th St. & Broadway in Lowertown.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.1051926,
+                44.9479189
+            ]
+        }
+    },
+
+
+    /**
+     * Cathedral Hill
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Cathedral Hill",
+            "img": "img/7182386698_8a9427eb49_b.jpg",
+            "creditLink": "https://flic.kr/p/bWFBkq",
+            "creditName": "jpellgen",
+            "creditLicense": "CC-BY-NC-ND-2.0",
+            "bodyCopy": "A short (uphill) walk delivers you to a great neighborhood with charming streets, wonderful restaurants and the stunning St. Paul Cathedral (step inside!) W. A. Frost has a sublime leafy patio and Moscow on the Hill has great Russian cuisine and an awesome selection of vodkas.  (Żubrówka flavored with bison grass!)",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.10913,
+                44.94691
+            ]
+        }
+    },
+
+
+    /**
+     * Summit Avenue
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Summit Avenue",
+            "img": "img/8031707780_6f10694d6e_b.jpg",
+            "creditLink": "https://flic.kr/p/deJAUm",
+            "creditName": "Holly Hayes",
+            "creditLicense": "CC-BY-NC-2.0",
+            "bodyCopy": "Rent a Nice Ride and tour one of the best preserved Victorian residential boulevards in the U.S. Level terrain and dedicated bike lanes make this a relaxing and easy bike route (once you climb up out of the downtown St. Paul gravity well). ",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.11520,
+                44.94284
+            ]
+        }
+    },
+
+
+    /**
+     * Urban Orchard
+     */
+    {
+        "type": "Feature",
+        "properties": {
+            "heading": "Urban Orchard",
+            "img": "img/28109150_3d4609af69_b.jpg",
+            "creditLink": "https://flic.kr/p/3u4Sw",
+            "creditName": "Bryan Costin",
+            "creditLicense": "CC-BY-SA-2.0",
+            "bodyCopy": "The Eleanor Graham Community Garden is home to St.  Paul’s first organic orchard – my pet project. Shameless self promotion! How unMinnesotan of me. WWGKT?  (What would Garrison Keillor Think?) If you get this far west, Keillor’s bookstore is just a few blocks away.",
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                -93.15614,
+                44.94420
+            ]
+        }
+    },
+
+    ]
+};
